2259bb4143e566944cd2e78516f4a0dac77f3ee1
Dawid Imbrzykowski <dawid.imb2010@gmail.com>
2025-04-21 13:51:04 +0200

    implemented full logic for downloading unzipping and replacing the themes

--- theme-switcher.sh
+++ theme-switcher.sh
@@ -5,6 +5,7 @@ TMP_DIR=$(mktemp -d -t theme-switcher.XXXXXX)
 trap "rm -rf \"$TMP_DIR\"" EXIT
 
 menu() {
+    clear
     echo "|============================|"
     echo "|           THEMES           |"
     echo "|============================|"
@@ -18,12 +19,15 @@ while true; do
     read -p "Choose an option: " choice
     case $choice in
         1)
-            echo "You chose Rias Gremory!"
-            # Add your theme change logic here
-            read -p "Press Enter to return to the menu..."
+            curl -L -s -o "$TMP_DIR/theme.zip" https://github.com/DawidDe/Arch-Themes/raw/refs/heads/main/rias-gremory/theme.zip
+            unzip "$TMP_DIR/theme.zip" -d "$TMP_DIR"
+            rm -r hypr wybar assets
+            mv $TMP_DIR/hypr ~/.config/hypr
+            mv $TMP_DIR/waybar ~/.config/waybar
+            mv $TMP_DIR/assets ~/.config/assets
             ;;
         2)
-            echo "Goodbye!"
+            clear
             exit 0
             ;;
         *)
