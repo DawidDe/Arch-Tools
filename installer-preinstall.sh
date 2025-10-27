eb2720fcdddb12338b198959f0c773a6c51a29d9
Dawid Imb <dawid.imb@gmail.com>
2025-10-27 11:58:56 +0100

    fixed typos

--- installer-preinstall.sh
+++ installer-preinstall.sh
@@ -19,16 +19,16 @@ echo "Set password for $username"
 read password
 
 # Prepare Partitions
-mkfs.fat -F 32 /dev/nvme0n1p
+mkfs.fat -F 32 /dev/nvme0n1p1
 mkswap /dev/nvme0n1p2
 mkfs.ext4 /dev/nvme0n1p3
 mount /dev/nvme0n1p3 /mnt
 mount --mkdir /dev/nvme0n1p1 /mnt/boot
-swapon /dev/nmve0n1p2
+swapon /dev/nvme0n1p2
 
 # Installing Base System
-pacstrap -K $rootmnt "${pacstrappacs[@]}"
-genfstab -U /mnt > /mnt/etc/fstab
+pacstrap -K /mnt "${pacstrappacs[@]}"
+genfstab -U /mnt >> /mnt/etc/fstab
 
 # Configurate System in chroot
 arch-chroot /mnt
